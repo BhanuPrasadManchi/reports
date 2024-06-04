95cf85df56cdb4c4562cf4d892137f9e3dd84011
Sai Krishna <m.s.krishna310@gmail.com>
2024-06-04 13:46:03 +0530

    added gotenberg sample code with okhttp

--- src/main/java/com/RTDMPL/thymeleaf/ReportsController.java
+++ src/main/java/com/RTDMPL/thymeleaf/ReportsController.java
@@ -1,15 +1,27 @@
 package com.RTDMPL.thymeleaf;
 
+import com.google.common.io.ByteStreams;
 import com.google.gson.Gson;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.text.DocumentException;
-import org.dom4j.rule.Mode;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import okhttp3.*;
+import org.apache.commons.io.IOUtils;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlPage;
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.W3CDom;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.io.*;
+import java.net.MalformedURLException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -34,10 +46,103 @@ public class ReportsController {
     RenderGraphService renderGraphService;
 
     @GetMapping("/report")
-    public String generatePdf(Model model) throws FileNotFoundException {
+    public String generatePdf(Model model) throws IOException {
         Context context = new Context();
-        String html = templateEngine.process("test", context);
-        HtmlConverter.convertToPdf(html, new FileOutputStream("src/main/resources/report.pdf"));
+        String html = templateEngine.process("test_dom", context);
+        System.out.println(html);
+//        HtmlConverter.convertToPdf(html, new FileOutputStream("src/main/resources/report.pdf"));
+//        try (final WebClient webClient = new WebClient()) {
+//            final HtmlPage page = webClient.getPage("https://msaikrishna.github.io/report-generation/");
+//            webClient.waitForBackgroundJavaScript(60_000);
+//            String domhtml = page.getWebResponse().getContentAsString();
+//            System.out.println("----DomHtml-----");
+//            System.out.println(domhtml);
+//            System.out.println("----DomHtmlEnd-----");
+//        } catch (Exception e) {
+//        }
+//        System.out.println("-----SeleniumIn----");
+//        WebDriver driver;
+//        ChromeOptions options=new ChromeOptions();
+//        options.addArguments("headless");
+//        driver=new ChromeDriver(options);
+//        driver.get("https://msaikrishna.github.io/report-generation/");
+//        String pageSource = driver.getPageSource();
+//        System.out.println(pageSource);
+//
+//        String filePath = "src/main/resources/dom_page_source.html";
+//
+//        try {
+//            File file = new File(filePath);
+//            FileWriter fileWriter = new FileWriter(file);
+//            fileWriter.write(pageSource);
+//            fileWriter.flush();
+//            fileWriter.close();
+//            System.out.println("HTML file written successfully.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//            ITextRenderer renderer = new ITextRenderer();
+//            renderer.setDocumentFromString(pageSource);
+//            renderer.layout();
+//            renderer.createPDF(new FileOutputStream("src/main/resources/report.pdf"), false);
+//            renderer.finishPDF();
+//
+//            Document document = Jsoup.parse(pageSource, "UTF-8");
+//            document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+//            PdfRendererBuilder builder = new PdfRendererBuilder();
+//            builder.withUri("src/main/resources/report_open.pdf");
+//            builder.toStream(new FileOutputStream("src/main/resources/report_open.pdf"));
+//            builder.withW3cDocument(new W3CDom().fromJsoup(document), "/");
+//            builder.run();
+
+        //Request from online data
+//        OkHttpClient client = new OkHttpClient();
+//        RequestBody requestBody = new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("url", "https://msaikrishna.github.io/report-generation")
+//                .build();
+//            Request request = new Request.Builder()
+//                    .url("http://localhost:12/forms/chromium/convert/url")
+//                    .post(requestBody)
+//                    .build();
+//         Response response = client.newCall(request).execute();
+//        InputStream pdfData = response.body().byteStream();
+//        try(FileOutputStream outputStream = new FileOutputStream("src/main/resources/report_from_gotenberg.pdf")){
+//            IOUtils.copy(pdfData, outputStream);
+//        } catch (FileNotFoundException e) {
+//            // handle exception here
+//        } catch (IOException e) {
+//            // handle exception here
+//        }
+
+        //Request from html data
+        OkHttpClient client = new OkHttpClient();
+        File testHtmlfile = new File("src/main/resources/templates/index.html");
+        File testJsfile = new File("src/main/resources/static/graph.js");
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("files", testHtmlfile.getName(), RequestBody.create(MediaType.parse("text/plain"), testHtmlfile))
+                .addFormDataPart("files", testJsfile.getName(), RequestBody.create(MediaType.parse("text/plain"), testJsfile))
+//                .addFormDataPart("paperWidth", "21cm")
+//                .addFormDataPart("paperHeight", "29.7cm")
+                .addFormDataPart("preferCssPageSize", "true")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:12/forms/chromium/convert/html")
+                .post(requestBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        InputStream pdfData = response.body().byteStream();
+        try(FileOutputStream outputStream = new FileOutputStream("src/main/resources/report_from_gotenberg_local.pdf")){
+            IOUtils.copy(pdfData, outputStream);
+        } catch (FileNotFoundException e) {
+            // handle exception here
+        } catch (IOException e) {
+            // handle exception here
+        }
+        System.out.println("-----SeleniumOut----");
+//        driver.quit();
         return "test";
     }
 
